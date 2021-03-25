5343b67bd225fa9bcdb321435d023c1b6ec2b875
Katkhade Dattatraya Jankiram <67061292+knight-furry@users.noreply.github.com>
2021-03-25 19:48:50 +0530

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,13 +1,14 @@
-#include <math.h>
 #include <stdio.h>
 
 void insertionSort(int arr[], int n)
 {
     int i, key, j;
-    for (i = 1; i < n; i++) {
+    for (i = 1; i < n; i++) 
+    {
         key = arr[i];
         j = i - 1;
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key) 
+        {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
@@ -15,20 +16,22 @@ void insertionSort(int arr[], int n)
     }
 }
  
-void printArray(int arr[], int n)
-{
-    int i;
-    for (i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
- 
 void main()
 {
-    int arr[] = { 12, 11, 13, 5, 6 };
-    int n = sizeof(arr) / sizeof(arr[0]);
- 
-    insertionSort(arr, n);
-    printArray(arr, n);
+    int a[100],i,n;
+	printf("Enter the size of array:\n");
+	scanf("%d",&n);
+	printf("Enter the element in array:\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	printf("\nThe given array is:\n");
+	for(i=0;i<n;i++)
+	printf("%d\t",a[i]);
+	printf("\nThe sorted array is:\n");
+	
+	insertionSort(a, n);
+	for(i=0;i<n;i++)
+	printf("%d\t",a[i]);
+	printf("\n");
  
 }
