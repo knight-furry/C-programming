33a64a6d9aa8212dd6867eeaebcd86d91d042b85
Katkhade Dattatraya Jankiram <67061292+knight-furry@users.noreply.github.com>
2021-07-17 22:03:39 +0530

    Create SkewSymetricMatrix.c

---
+++ 2D arrays or matrices/SkewSymetricMatrix.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+void main()
+{
+	int a[3][3],m,n,i,j,k,m1,n1,b[3][3],flag=0,f=0;
+	printf("enter size of matrix (m*n):\n");
+	scanf("%d%d",&m,&n);
+	printf("enter the elements:\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+		printf("[%d][%d]=",i,j);
+		scanf("%d",&a[i][j]);
+		}
+	}
+	
+	printf("The matrix a is:\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		printf("%d\t",a[i][j]);
+		printf("\n");
+	}
+	if(m==n)
+	{
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(i==j)
+			{
+				if(a[i][j]!=1)
+				f++;
+			}
+			else
+			{
+				if(a[i][j]!=(-a[j][i]))
+				flag++;
+			}
+		}
+	}
+		if(f==0 && flag==0)
+		printf("The given matrix is skew symetric matrix....\n");
+		else
+		printf("The given matrix is not skew symetric matrix.....\n");
+	}
+	else
+	printf("Matrix does not having skew symetric matrix.......\n");
+}
