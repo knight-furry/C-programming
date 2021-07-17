f6df44b99cf86c152f615c4ebd886dc7a5074ca0
Katkhade Dattatraya Jankiram <67061292+knight-furry@users.noreply.github.com>
2021-07-17 22:01:53 +0530

    Create MatrixIdentity.c

---
+++ 2D arrays or matrices/MatrixIdentity.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void main()
+{
+	int a[3][3],m,n,i,j,k,m1,n1,b[3][3],flag=0,f=0;
+	printf("enter size of matrix (m*n):\n");
+	scanf("%d%d",&m,&n);
+	printf("enter the elements:\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		scanf("%d",&a[i][j]);
+	}
+	
+	printf("The matrix a is:\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		printf("%d\t",a[i][j]);
+		printf("\n");
+	}
+	if(m==n)
+	{
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(i==j)
+			{
+				if(a[i][j]!=1)
+				f++;
+			}
+			else
+			{
+				if(a[i][j]!=0)
+				flag++;
+			}
+		}
+	}
+		if(f==0 && flag==0)
+		printf("The given matrix is identity matrix....\n");
+		else
+		printf("The given matrix is not identity matrix.....\n");
+	}
+	else
+	printf("Matrix does not having identity matrix.......\n");
+}
